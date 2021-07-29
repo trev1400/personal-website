c0886d3e97569259e52059b092544449164df73a
trev1400 <trevfix@gmail.com>
2021-07-28 23:17:20 -0700

    Updates

--- precache-manifest.b2da2bd73eded012f56d6bf24b70398a.js
+++ precache-manifest.b2da2bd73eded012f56d6bf24b70398a.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a2847062c7cc6578c9d7a2bb8acdab97",
+    "revision": "bc45d83ed45bab89eaf50068cb7fd80e",
     "url": "/personal-website/index.html"
   },
   {
-    "revision": "619da9fd7adea02aef8a",
+    "revision": "fa28849a5c133c4f172a",
     "url": "/personal-website/static/css/main.2bb1e0fe.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/personal-website/static/js/2.cfa9fcf5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "619da9fd7adea02aef8a",
-    "url": "/personal-website/static/js/main.e51005a4.chunk.js"
+    "revision": "fa28849a5c133c4f172a",
+    "url": "/personal-website/static/js/main.ea2819cc.chunk.js"
   },
   {
     "revision": "d9385b05e5e57d9628fe",
@@ -42,10 +42,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "d31ae62cda16e1554f1aaf976ce0bf2d",
     "url": "/personal-website/static/media/mootual.d31ae62c.png"
-  },
-  {
-    "revision": "81175c014b6b66f74f335789a8349c3c",
-    "url": "/personal-website/static/media/pal.81175c01.png"
   },
   {
     "revision": "8184bb7037d9df53511ebac778ec7bb8",
